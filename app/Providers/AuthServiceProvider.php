edefe396c64e007e67c49719c74057e16120f0d0
minuschuman <sthachuman@gmail.com>
2023-02-22 14:12:34 +0545

    Grant Super-Admin access

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,5 +26,10 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        Gate::before(function ($user, $ability) {
+            if ($user->hasRole(env('APP_SUPER_ADMIN', 'super-admin'))) {
+                return true;
+            }
+        });
     }
 }
