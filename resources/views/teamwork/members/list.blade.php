adfb1ef32b641ccc5def8a35c88707c00567e851
minuschuman <sthachuman@gmail.com>
2023-03-02 15:35:34 +0545

    update view

--- resources/views/teamwork/members/list.blade.php
+++ resources/views/teamwork/members/list.blade.php
@@ -10,7 +10,7 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="px-6 flex justify-between items-center">
                         <h2
-                            class="inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 py-4 block sm:inline-block flex">
+                            class="text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 py-4 sm:inline-block flex">
                             Members of team <span class="tracking-wide">{{ $team->name }}</span></h2>
                         <a href="{{ route('teams.index') }}"
                             class="px-4 py-2 text-white mr-4 bg-blue-600">{{ __('Back to Teams') }}</a>
@@ -30,20 +30,34 @@
                                 </div>
                             @endif
                             <div class="min-w-full border-b border-gray-200 shadow">
-                                <form method="GET" action="{{ route('teams.members.show', ['id' => $team->id]) }}">
-                                    <div class="pb-2 pt-4 flex">
-                                        <div class="overflow-hidden flex pl-4">
-                                            <input type="search" name="search"
-                                                value="{{ request()->input('search') }}"
-                                                class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                                placeholder="Search">
-                                            <button type='submit'
-                                                class='ml-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150'>
-                                                {{ __('Search') }}
-                                            </button>
-                                        </div>
+                                <div class="flex justify-between">
+                                    <div>
+                                        @can('user create')
+                                            <div class="py-2 flex">
+                                                <div class="overflow-hidden flex pl-4">
+                                                    <a href="{{ route('teams.user.add', ['team' => $team->id]) }}"
+                                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{ __('Add Member') }}</a>
+                                                </div>
+                                            </div>
+                                        @endcan
                                     </div>
-                                </form>
+                                    <form method="GET" class="flex-end"
+                                        action="{{ route('teams.members.index', ['team' => $team->id]) }}">
+                                        <div class="py-2 flex">
+                                            <div class="overflow-hidden flex pr-4">
+                                                <input type="search" name="search"
+                                                    value="{{ request()->input('search') }}"
+                                                    class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                                    placeholder="Search">
+                                                <button type='submit'
+                                                    class='ml-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150'>
+                                                    {{ __('Search') }}
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
                                 <table class="border-collapse table-auto w-full text-sm">
                                     <thead>
                                         <tr
@@ -68,8 +82,7 @@
                                                 </td>
                                                 <td
                                                     class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                                    {{-- {{ App\Models\User::findOrFail($user->id)->roles->first()->name ?? ' ' }} --}}
-                                                    {{ $user->roles->first()->name ?? 'Guest' }}
+                                                    {{ $user->role }}
                                                 </td>
                                                 <td
                                                     class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
@@ -99,7 +112,11 @@
                         </div>
                     </div>
                 </div>
-                {{-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            </div>
+        </div>
+    </div>
+
+    {{-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="w-full px-6 py-4 bg-white overflow-hidden">
                 @if (auth()->user()->isOwnerOfTeam($team))
@@ -112,14 +129,21 @@
                                 <thead>
                                     <tr
                                         class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase bg-gray-50 border-b">
-                                        <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-left">E-Mail</th>
-                                        <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-left">Action</th>
+                                        <th
+                                            class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-left">
+                                            E-Mail</th>
+                                        <th
+                                            class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-left">
+                                            Action</th>
                                     </tr>
                                 </thead>
                                 @foreach ($team->invites as $invite)
                                     <tr class="text-gray-700">
-                                        <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $invite->email }}</td>
-                                        <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                        <td
+                                            class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                            {{ $invite->email }}</td>
+                                        <td
+                                            class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
                                             <a href="{{ route('teams.members.resend_invite', $invite) }}"
                                                 class="inline-flex px-4 py-2 text-sm font-medium text-white bg-purple-600 rounded-lg border border-transparent active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring">
                                                 Resend invite
@@ -131,7 +155,8 @@
                         </div>
                     </div>
 
-                    <h3 class="mb-3 text-lg font-semibold tracking-wide">Invite to team "{{ $team->name }}"
+                    <h3 class="mb-3 text-lg font-semibold tracking-wide">Invite to team
+                        "{{ $team->name }}"
                     </h3>
 
                     <form class="form-horizontal" method="post" action="{{ route('teams.members.invite', $team) }}">
